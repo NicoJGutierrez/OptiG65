5b751e2c3c75f7ce18ff9d450a3db56bc9650c87
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-10-25 09:17:05 -0300

    feat: estaciones + no ciclos de 2 nodos

--- main.py
+++ main.py
@@ -10,11 +10,27 @@ model = Model()
 
 # Se instancian variables de decision
 
-#Existe conexión directa entre 2 estaciones?
+# Existe conexión directa entre 2 estaciones?
+x = {}
 for i in nodos:
     for j in nodos:
         if i != j: # Restricción de nodos no iguales
-            x = model.addVar(vtype = GRB.BINARY, name="x_({i[0]}, {j[0]})") # 0 tiene que ser el nombre de la ciudad
+            x[i,j] = model.addVar(vtype = GRB.BINARY, name="x_({i[0]}, {j[0]})") # 0 tiene que ser el nombre de la ciudad
+            # x = conexión
+
+# Evitar ciclos de 2 nodos (los arcos son bidireccionales)
+for i in nodos:
+    for j in nodos:
+        if i != j:
+            model.addConstr(x[i, j] + x[j, i] <= 1, f"no_ciclo_{i}_{j}")
+
+# Hay una estación en un nodo i?
+y = {}
+for i in nodos:
+    y[i] = model.addVar(vtype = GRB.BINARY, name="y_({i[0]})") # y = estación
+
+
+z = model.addVar(vtype = GRB.CONTINUOUS, name="z")
 
 # Llamamos a update
 # Restricciones
