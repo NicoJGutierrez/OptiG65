e40e21ad69e13b3868f0cc24ce9376e7631b4229
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-10-25 15:35:08 -0300

    feat: constraint de presupuesto

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ presupuesto = input("Elija un presupuesto para el proyecto (en miles de pesos)")
 nodos = CSV.leer_csv()
 # 0 = id, 1 = nombre, 2 = precio estación, 3 = población, 4...n = precio de armar un arco entre el nodo de id (i-2) y este.
 # Los precios deben estar en miles de pesos
+# IMPORTANTE: LOS COSTOS REPETIDOS DEBEN SER 0.
 
 # Generar el modelo
 model = Model()
@@ -47,10 +48,23 @@ for i in nodos:
         if i != j:
             model.addConstr(x[i, j] + x[j, i] <= 1, f"no_ciclo_{i[1]}_{j[1]}")
 
+# El costo total es menor al presupuesto
+model.addConstr(
+    (quicksum(x[i, j] * (i[j[0] + 4])) for i, j in nodos if i != j) # Conexiones
+    + (quicksum(y[i]*i[2]) for i in nodos) # Estaciones
+    <= presupuesto, f"Costos")
+# Las conexiones no se generan dos veces porque se asume que las conexiones repetidas valen 0
+# y aparece aplicada la restricción de que no generen loops
+
+
+# Se cumple que de un nodo conectado puedo tirar una señal igual a los nodos conectados menos uno
+# y esa señal va a ser demandada por todos los demás nodos.
+
 
 # Funcion Objetivo y optimizar el problema:
 
-model.setObjective(quicksum((y[i] * i[3]) for i in nodos), GRB.MAXIMIZE)
+model.setObjective(quicksum((y[i] * i[3]) for i in nodos), GRB.MAXIMIZE) 
+#maximizar la suma de las estaciones puestas por la población en ellas
 
 # Falta la función objetivo
 model.optimize()
