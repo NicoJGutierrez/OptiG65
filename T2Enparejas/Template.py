01ceabe9da24ae7c9b4af561f6e9067729a6cba4
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-11-02 22:00:28 -0300

    feat: copiada sintaxis básica

--- T2Enparejas/Template.py
+++ T2Enparejas/Template.py
@@ -9,9 +9,12 @@ S = range(10)
 E = range(37)
 
 ## Añadir las resrticciones
+for e in E:
+    for s in S:
+        model.addConstr(quicksum(FLOW[j[0], i[0]] for j in nodos if i != j) - y[i[0]] == quicksum(FLOW[i[0], j[0]] for j in nodos if i != j), f"Nodo_receptor_${i[1]}")
 
 # Añadir función objetivo
-
+model.setObjective(quicksum((y[i[0]] * i[3]) for i in nodos), GRB.MAXIMIZE) 
 
 model.update()
 model.optimize()
