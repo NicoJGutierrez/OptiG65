c98442b1b0b60d904d2f8cc960ba55ddaf073fd6
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2023-11-03 17:00:35 -0300

    feat: variables y func de optimizacion

--- T2Enparejas/Template.py
+++ T2Enparejas/Template.py
@@ -8,13 +8,28 @@ T = range(25)
 S = range(10)
 E = range(37)
 
+## Añadir las variables
+x = {}
+y = {}
+z = {}
+# Se asigna el empleado (e) a una función (s) el día (t)?
+for e in E:
+    for t in T:
+        for s in S:
+            x[e,t,s] = model.addVar(vtype = GRB.BINARY, name=f"x_{e}_{t}_{s}")
+    y[e] = model.addVar(vtype = GRB.BINARY, name=f"y_{e}")
+    z[e] = model.addVar(vtype = GRB.BINARY, name=f"z_{e}")
+
 ## Añadir las resrticciones
 for e in E:
     for s in S:
-        model.addConstr(quicksum(FLOW[j[0], i[0]] for j in nodos if i != j) - y[i[0]] == quicksum(FLOW[i[0], j[0]] for j in nodos if i != j), f"Nodo_receptor_${i[1]}")
+        for t in T:
+            model.addConstr(x[e,t,s] - y[i[0]] <= h[e,s], f"Nodo_receptor_${i[1]}")
 
 # Añadir función objetivo
-model.setObjective(quicksum((y[i[0]] * i[3]) for i in nodos), GRB.MAXIMIZE) 
+model.setObjective(quicksum((c[e,t,s] * x[e,t,s]) for e in E for t in T for s in S) 
+                   + quicksum((f[e] * y[e]) for e in E)
+                   + quicksum((d[e] * z[e]) for e in E), GRB.MINIMIZE) 
 
 model.update()
 model.optimize()
